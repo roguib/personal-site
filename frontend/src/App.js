065708e5c318763eef0ac829828a31822fa38579
roguib <roger.guib@gmail.com>
2021-07-08 12:18:47 +0200

    title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="title">
+        <h1>Hi,</h1>
+        <h1>I'm Roger Guasch I Ibarra,</h1>
+        <h1>A Software Engineer</h1>
+      </div>
     </div>
   );
 }
