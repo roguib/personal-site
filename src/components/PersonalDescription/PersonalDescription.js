ce4059d1e38a0f2a175b2e6e055e2dfc4aa7df0f
roguib <rgrguib@gmail.com>
2024-08-17 19:19:05 +0200

    add personal description

---
+++ src/components/PersonalDescription/PersonalDescription.js
@@ -0,0 +1,13 @@
+import "./PersonalDescription.css";
+
+function PersonalDescription() {
+    return(
+        <div className="personal-description-wrapper">
+            <p>In 2021, my journey led me from the vibrant streets of Barcelona to the enchanting city of Prague, spurred by the opportunity to join Oracle as a Software Development Engineer. As a JavaScript developer, my passion lies in crafting resilient and easily maintainable code, intertwined with seamless collaboration within a diverse and expansive team.</p>
+            <p>At present, my focus revolves around contributing to the Visual Builder Studio, an innovative cloud-based platform designed for application development. Here, I thrive in an environment that fosters creativity and efficiency, working alongside a global, multicultural team to create cutting-edge solutions.</p>
+            <p>My dedication to the JavaScript craft encompasses not only writing robust code but also ensuring its longevity and manageability. I am enthusiastic about leveraging my skills to enhance the Visual Builder Studio, contributing to its growth and success in the realm of cloud-based software development.</p>
+        </div>
+    );
+}
+
+export default PersonalDescription;
