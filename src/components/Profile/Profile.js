e061e3d99ed336924f5ea23a7ed8a87b21662cdc
roguib <rgrguib@gmail.com>
2024-08-19 21:42:51 +0200

    use a placeholder for profile pic

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,10 +1,14 @@
 import './Profile.css';
+import profilePicture from '../../images/profile-picture.png';
 
 function Profile() {
     return (
         <div className="profile-wrapper">
-            <p>Roger Guasch Ibarra</p>
-            <p>Software Developer Engineer</p>
+            <img src={profilePicture} />
+            <div className="name-wrapper">
+                <p>Roger Guasch Ibarra</p>
+                <p>Software Developer Engineer</p>
+            </div>
         </div>
     );
 }
