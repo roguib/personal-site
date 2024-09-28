8ee66b102d22af655ab19b1538293e2abe9866ee
roguib <rgrguib@gmail.com>
2024-09-28 12:32:53 +0200

    finish theme button animation

--- src/components/ThemeButton/ThemeButton.js
+++ src/components/ThemeButton/ThemeButton.js
@@ -1,30 +1,50 @@
 import { useEffect, useRef } from 'react';
 import ThemeUtils from "../../utils/ThemeUtils";
+// TODO: convert them to svg for faster loading
+import moonImg from "../../images/moon.png";
+import sunImg from "../../images/sun.png";
 import "./ThemeButton.css";
 
 function ThemeButton() {
     const themeButtonRef = useRef(null);
+    const themeIconsWrapperRef = useRef(null);
     
     useEffect(() => {
         const themeOptions = ThemeUtils.THEME_OPTIONS;
         if (!ThemeUtils.prefersDarkTheme()) {
-            themeButtonRef.current.className = themeOptions.LIGHT;
+            // themeButtonRef.current.className = themeOptions.LIGHT;
+            themeButtonRef.current.className += ` theme-button-wrapper-initial-pos-${themeOptions.LIGHT}`;
+            themeIconsWrapperRef.current.className += ` initial-${themeOptions.LIGHT}-background`;
             ThemeUtils.updateThemePreferences(themeOptions.LIGHT);
             return;
         }
         ThemeUtils.updateThemePreferences(themeOptions.DARK);
+        themeIconsWrapperRef.current.className += ` initial-${themeOptions.DARK}-background`;
     });
 
     const handleThemeSwitch = () => {
         const themeOptions = ThemeUtils.THEME_OPTIONS;
         let themeChoice;
-        if (!!!themeButtonRef.current.className ||
-            themeButtonRef.current.className.indexOf(themeOptions.DARK) === 0) {
+        if (!!!themeButtonRef.current.className || ThemeUtils.hasClass(themeButtonRef, themeOptions.DARK)) {
             themeChoice = themeOptions.LIGHT;
             themeButtonRef.current.className = themeChoice;
+            if (ThemeUtils.hasClass(themeIconsWrapperRef, `initial-${themeOptions.DARK}-background`)) {
+                ThemeUtils
+                    .replaceOrAppendClass(themeIconsWrapperRef, `initial-${themeOptions.DARK}-background`, `${themeOptions.LIGHT}-background`);
+            } else {
+                ThemeUtils
+                    .replaceOrAppendClass(themeIconsWrapperRef, `${themeOptions.DARK}-background`, `${themeOptions.LIGHT}-background`);
+            }
         } else {
             themeChoice = themeOptions.DARK;
             themeButtonRef.current.className = themeChoice;
+            if (ThemeUtils.hasClass(themeIconsWrapperRef, `initial-${themeOptions.LIGHT}-background`)) {
+                ThemeUtils
+                    .replaceOrAppendClass(themeIconsWrapperRef, `initial-${themeOptions.LIGHT}-background`, `${themeOptions.DARK}-background`);
+            } else {
+                ThemeUtils
+                    .replaceOrAppendClass(themeIconsWrapperRef, `${themeOptions.LIGHT}-background`, `${themeOptions.DARK}-background`);
+            }
         }
         ThemeUtils.updateThemePreferences(themeChoice);
     };
@@ -32,6 +52,10 @@ function ThemeButton() {
     return (
         <div class="theme-button-wrapper">
             <button ref={themeButtonRef} onClick={handleThemeSwitch}></button>
+            <div ref={themeIconsWrapperRef} class="theme-icons-wrapper">
+                <img src={sunImg} />
+                <img src={moonImg} />
+            </div>
         </div>
     )
 }
--- src/utils/ThemeUtils.js
+++ src/utils/ThemeUtils.js
@@ -30,6 +30,37 @@ export class ThemeUtils {
         localStorage.setItem(THEME_KEY, preferedTheme);
     }
 
+    /**
+     * A utility method that checks if classA exists in ref element to replace it with classB,
+     * if it doesn't exist it appends classB to the list of classes the ref has
+     * 
+     * @param {React.useRef} ref The reference to the element whose class is going to get updated
+     * @param {*} classA The class that should be replaced, in case it exists
+     * @param {*} classB The class that needs to be set
+     */
+    static replaceOrAppendClass(ref, classA, classB) {
+        if (ref.current.className.indexOf(classA) >= 0) {
+            ref.current.className = ref.current.className
+                .replace(classA, classB);
+        } else {
+            ref.current.className += ` ${classB}`;
+        }
+    };
+
+    /**
+     * Returns true if ref contains classToSearch, false otherwise
+     *
+     * @param {React.useRef} ref 
+     * @param {string} classToSearch 
+     * @returns {boolean}
+     */
+    static hasClass(ref, classToSearch) {
+        return ref
+            .current
+            .className
+            .indexOf(classToSearch) >= 0;
+    }
+
     static get THEME_OPTIONS() {
         return THEME_OPTIONS;
     }
