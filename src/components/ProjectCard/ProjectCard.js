6fa8da7c000f46385af63fd02ef3db56c2fcb414
roguib <rgrguib@gmail.com>
2024-10-19 18:34:32 +0200

    update ProjectCard to the new design

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -3,9 +3,12 @@ import "./ProjectCard.css";
 function ProjectCard({ title, description, anchors, image }) {
     return (
         <div className="card">
+            <div style={{ display: 'flex', 'justify-content': 'center' }}>
+                <img className="card-image" src={image} />
+            </div>
             <h3>{title}</h3>
             <div className="card-body">
-                <p className="card-text" align="justify">{description}</p>
+                <p align="justify">{description}</p>
             </div>
             <>
                 {anchors.map(({ link, text }) => (
